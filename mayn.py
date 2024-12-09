a1368040c1c1da7a9eda32d7df65427c4048268d
sudipkarki123 <sudipkarkicoding@gmail.com>
2024-12-09 18:43:35 +0545

    lesson 4

---
+++ mayn.py
@@ -0,0 +1,23 @@
+# a=98
+# b=94
+# c=41
+# d=96
+# e=20
+# sum=a+b+c+d+e
+# average=sum/5
+# print("average height of trees=",average)
+
+# Amount =int(input("please Enter Amount for Withdraw:"))
+# note_1=Amount//100
+# note_2=(Amount%100)//50
+# note_3=((Amount%100)%50)//10
+# print("notes of 100 rs", note_1)
+# print("notes of 50 rs", note_2)
+# print("notes of 10 rs", note_3)
+
+maths=int(input("Enter math marks(out of 50);"))
+english=int(input("Enter english marks(out of 50);"))
+eco=int(input("Enter eco marks(out of 50);"))
+total=maths+english+eco
+perc=total/150*100
+print("percentage:",perc)
